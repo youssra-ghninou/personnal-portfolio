b5d3fcea1ae8f6ab6fab9e31f82d5ec0bce7aefd
youssra-ghninou <ghninou177u@gmail.com>
2022-08-31 16:56:43 +0100

    components added

---
+++ components/Github.jsx
@@ -0,0 +1,14 @@
+const Github = () => {
+    return ( 
+        <div>
+        <button className="bg-violet rounded-sm w-[80px] h-12">
+            <div className="font-abel text-center justify-center items-center text-sm">
+              GITHUB
+            </div>
+        </button>
+    </div>
+
+     );
+}
+ 
+export default Github;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,18 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors:{
+      black:'000000',
+      violet:'756BF0',
+      blue:'35FEFF',
+      yellow:'FFE385',
+      white:'F7F7F7',
+      green:'5CDFA5',
+      dviolet:'484575'
+    },
+    fontFamily:{
+      'abel':['Abel', 'sans-serif']
+    }
   },
   plugins: [],
 }
