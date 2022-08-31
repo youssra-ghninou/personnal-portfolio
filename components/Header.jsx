8029f6c2118a07768ce2e5336f58b484cd42b926
youssra-ghninou <ghninou177u@gmail.com>
2022-08-31 19:48:59 +0100

    :(

---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (  
+        <div className="container">
+
+            <div className="name"> - YOUSSRA GHNINOU</div>
+            <div className="profile">I’m a software developer in the bay area who enjoys designing and building beautiful, performant, responsive websites </div>
+        </div>
+    );
+}
+ 
+export default Header;
